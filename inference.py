b6e57fe6f154759f28f8da8df37369c7b41974a5
David NAISSE <private.david.naisse@gmail.com>
2021-10-13 17:50:40 +0200

    added inference script

---
+++ inference.py
@@ -0,0 +1,14 @@
+import numpy as np
+from torchvision.transforms import ToTensor
+
+from model import Model
+
+# Load data & dataloader
+train_data, test_data, train_dataloader, test_dataloader = Model.load_mnist(transform=ToTensor(), batch_size=1)
+
+# Load model
+model = Model(load_model='model/model.pt', img_chan_size=100, global_chan_size=50)
+print(f'Trainable parameters: {sum(p.numel() for p in model.model.parameters())}. ')
+
+# Quick inference
+model.infer(eval_data=test_data, random=True)
