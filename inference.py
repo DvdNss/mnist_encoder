4e60945e3062d9e754f081876d4635b00b9319f7
DvdNss <private.david.naisse@gmail.com>
2021-11-17 07:12:31 +0100

    added global evaluation

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 from torchvision.transforms import ToTensor
+from tqdm import tqdm
 
 from model import Model
 
@@ -9,5 +10,13 @@ train_data, test_data, train_dataloader, test_dataloader = Model.load_mnist(tran
 model = Model(load_model='model/model.pt', img_chan_size=100, global_chan_size=50)
 print(f'Trainable parameters: {sum(p.numel() for p in model.model.parameters())}. ')
 
-# Quick inference
-model.infer(eval_data=test_data, random=True)
+# # Quick inference
+# model.infer(eval_data=test_data, random=True)
+
+# Evaluation
+correct = 0
+for _ in tqdm(range(0, len(test_data))):
+    target, pred, rec_pred = model.infer(eval_data=test_data, n_example=_, random=False)
+    correct += 1 if pred == target else 0
+
+print(f'Total accuracy: {correct / len(test_data) * 100}%. ')
